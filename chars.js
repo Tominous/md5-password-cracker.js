5ee87155c40329716a46e709b875fc428bcc6247
Tominous <18189490+Tominous@users.noreply.github.com>
2019-10-21 19:04:50 +0100

    Update chars.js

--- chars.js
+++ chars.js
@@ -1,12 +1,12 @@
 // Try all alpha-numeric passwords (lower and uppercase)
-// var alpha = "abcdefghijklmnopqrstuvwxyz"
-// var num = "0123456789"
-// var chars = [];
-// chars.push.apply(chars, alpha.split(""))
-// chars.push.apply(chars, alpha.toUpperCase().split(""))
-// chars.push.apply(chars, num.split(""))
+ var alpha = "abcdefghijklmnopqrstuvwxyz"
+ var num = "0123456789"
+ var chars = [];
+ chars.push.apply(chars, alpha.split(""))
+ chars.push.apply(chars, alpha.toUpperCase().split(""))
+ chars.push.apply(chars, num.split(""))
 
-// var charsLength = chars.length
+ var charsLength = chars.length
 
 
 // Letters (upper and lower) and numbers
@@ -18,11 +18,11 @@ var from = 48
   , skip2_to = 97
 
 // Letters (upper and lower), numbers, and special characters 
-//var from = 33
+var from = 33
 //  , to = 127; /* one past the end */
 
-// var chars = []
-// for (var i = from; i < to; i++) {
-//   chars.push(String.fromCharCode(i))
-// }
-// chars = chars.join('')
+ var chars = []
+ for (var i = from; i < to; i++) {
+   chars.push(String.fromCharCode(i))
+ }
+ chars = chars.join('')
